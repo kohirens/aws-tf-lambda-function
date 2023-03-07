2209225af663945b058b61b87946201fa2ac1557
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2023-03-06 22:03:26 -0500

    test: Updated To Check Lambda And Log Group Deployments

--- tests/default/main.tf
+++ tests/default/main.tf
@@ -17,6 +17,7 @@ terraform {
 
 locals {
   aws_region = "us-west-1"
+  name       = "terraform-test"
 }
 
 provider "aws" {
@@ -28,11 +29,21 @@ module "main" {
   aws_account            = local.aws_account
   aws_region             = local.aws_region
   iac_source             = "terratest.fakehub.com"
-  name                   = "terraform-test"
+  name                   = local.name
   runtime                = "go1.x"
   url_authorization_type = "NONE"
   source_file            = "tests/default/hello"
   handler                = "hello"
+}
+
+data "aws_lambda_function" "existing" {
+  depends_on    = [module.main]
+  function_name = local.name
+}
+
+data "aws_cloudwatch_log_group" "existing" {
+  depends_on = [module.main]
+  name       = "lambda-${local.name}"
 }
 
 resource "test_assertions" "deploy_iam_policy_user_and_role" {
@@ -48,5 +59,17 @@ resource "test_assertions" "deploy_iam_policy_user_and_role" {
     description = "iam role deploy"
     got         = module.main.iam_role_arn
     want        = "arn:aws:iam::755285156183:role/lambda-terraform-test-role"
+  }
+
+  equal "function_deployed" {
+    description = "lambda-${local.name} deploy"
+    got         = data.aws_lambda_function.existing.arn
+    want        = "arn:aws:lambda:us-west-1:755285156183:function:terraform-test"
+  }
+
+  equal "log_group_deployed" {
+    description = "lambda-${local.name} log group deploy"
+    got         = data.aws_cloudwatch_log_group.existing.arn
+    want        = "arn:aws:logs:us-west-1:755285156183:log-group:lambda-terraform-test"
   }
 }

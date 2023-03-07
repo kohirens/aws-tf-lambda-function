4d5921830719b37c5c0c5744d8b5ebe3af8a85b9
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2023-03-06 21:59:56 -0500

    add: More Outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,15 @@ output "iam_policy_arn" {
 output "iam_role_arn" {
   value = length(aws_iam_role.main) > 0 ? aws_iam_role.main[0].arn : null
 }
+
+output "function" {
+  value = aws_lambda_function.main.arn
+}
+
+output "function_memory_size" {
+  value = aws_lambda_function.main.memory_size
+}
+
+output "log_group" {
+  value = aws_cloudwatch_log_group.main.arn
+}

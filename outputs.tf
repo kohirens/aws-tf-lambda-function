3690e0a8f91ac9bea3a2db6071a8df85ba184eea
Khalifah K. Shabazz <shabazzk@gmail.com>
2023-11-08 07:14:43 -0500

    add: IAM Role Name Output

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "iam_role_arn" {
   value = length(aws_iam_role.main) > 0 ? aws_iam_role.main[0].arn : null
 }
 
+output "iam_role_name" {
+  value = length(aws_iam_role.main) > 0 ? aws_iam_role.main[0].name : null
+}
+
 output "function_arn" {
   value = aws_lambda_function.main.arn
 }
